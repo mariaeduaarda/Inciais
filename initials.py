b60b9da9fbe95f39440104869741a8e5ddf59a9e
mariaeduaarda <74739628+mariaeduaarda@users.noreply.github.com>
2023-10-19 18:41:41 -0300

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,9 +1,9 @@
 # Maria Eduarda
 
-print = (" M           M     E E E E E E ")
-print = (" M  M     M  M     E           ")
-print = (" M     M     M     E           ")
-print = (" M           M     E E E E     ")
-print = (" M           M     E           ")
-print = (" M           M     E           ")
-print = (" M           M     E E E E E E ")
+print(" M           M     E E E E E E ")
+print(" M  M     M  M     E           ")
+print(" M     M     M     E           ")
+print(" M           M     E E E E     ")
+print(" M           M     E           ")
+print(" M           M     E           ")
+print(" M           M     E E E E E E ")
