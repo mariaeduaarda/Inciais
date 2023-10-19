ed6d43549afddfd70e1a86a45b576bab280bf852
mariaeduaarda <74739628+mariaeduaarda@users.noreply.github.com>
2023-10-19 18:34:45 -0300

    initials.py

---
+++ initials.py
@@ -0,0 +1,10 @@
+# Maria Eduarda
+# Magalhães
+
+print = ("" M           M     E E E E E E "")
+print = ("" M  M     M  M     E           "")
+print = ("" M     M     M     E           "")
+print = ("" M           M     E E E E     "")
+print = ("" M           M     E           "")
+print = ("" M           M     E           "")
+print = ("" M           M     E E E E E E "")
